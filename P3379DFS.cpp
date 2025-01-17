d311fd726fde49a32632e083d48ce5c6721c56d1
huangminzhe <Lin_morning@outlook.com>
2025-01-17 16:01:55 +0800

    P3379LCA模板DFS序完成

--- P3379DFS.cpp
+++ P3379DFS.cpp
@@ -1,41 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 5e5 + 5;
-int n,m,s;
-vector<int> a[N];
-int dep[N],fa[N],d[N],dfn[N],t;	// 层数；父亲；DFS序；时间戳；时间总和
-void dfs(int u){
-	dfn[u] = ++t,d[t] = u;
-	for (int v : a[u]){
-		if (v != fa[u]){
-			dep[v] = dep[u] + 1;
-			fa[v] = u;
-			dfs(v);
+const int N = 5e5 + 5,LOG2N = 20;
+class LCA{
+	private:
+		vector<int> a[N];	// 图
+		int dep[N],fa[N];	// 层数；父节点
+		int d[N],dfn[N],t;	// DFS序；时间戳；时间总和
+		int Log2[N],st[N][LOG2N];	// 2的幂次；区间深度最小的结点的ST表
+		inline int get(int u,int v){return dep[u] < dep[v]?u:v;}
+		void dfs(int u){
+			dfn[u] = ++t,d[t] = u,st[t][0] = fa[u];
+			for (int v : a[u]){
+				if (v != fa[u]){
+					dep[v] = dep[u] + 1;
+					fa[v] = u;
+					dfs(v);
+				}
+			}
 		}
-	}
-}
-int lca(int u,int v){
-	// 确保u在v左边（或上面）
-	if (dfn[u] > dfn[v])	swap(u,v);
+	public:
+		LCA(){
+			t = 0;
+		}
+		void init(int n,int s){
+			dep[s] = 1;
+			dfs(s);
 
-	int v_ = v;
-	for (int i = dfn[u] + 1;i <= dfn[v];i++)
-		if (dep[d[i]] < dep[v_])	v_ = d[i];
-	return fa[v_];
-}
+			for (int i = 2;i <= n;i++)
+				Log2[i] = Log2[i >> 1] + 1;
+			for (int j = 1;j <= Log2[n];j++)
+				for (int i = 1;i <= n - (1 << j) + 1;i++)
+					st[i][j] = get(st[i][j - 1],st[i + (1 << (j - 1))][j - 1]);
+		}
+		void push(int u,int v){
+			a[u].push_back(v);
+			a[v].push_back(u);
+		}
+		int lca(int u,int v){
+			if (dfn[u] > dfn[v])	swap(u,v);
+
+			if (u == v)	return u;
+			int k = Log2[dfn[v] - dfn[u]];	// u+1~v
+			return get(st[dfn[u] + 1][k],st[dfn[v] - (1 << k) + 1][k]);
+		}
+};
+int n,m,s;
+LCA a;
 int main(int argc, char **argv){
 	cin >> n >> m >> s;
 	int u,v;
 	for (int i = 1;i < n;i++){
 		scanf("%d %d",&u,&v);
-		a[u].push_back(v);
-		a[v].push_back(u);
+		a.push(u,v);
 	}
-	dep[s] = 1;
-	dfs(s);
+	a.init(n,s);
 	while (m--){
 		scanf("%d %d",&u,&v);
-		printf("%d\n",lca(u,v));
+		printf("%d\n",a.lca(u,v));
 	}
 	return 0;
 }
