1fb397f4640e3cbe19db1f9e5073b51f93f191b6
huangminzhe <Lin_morning@outlook.com>
2025-02-25 20:10:05 +0800

    完成P1731生日蛋糕

--- Luogu/P1731.cpp
+++ Luogu/P1731.cpp
@@ -13,7 +13,7 @@ void dfs(int x,int v,int s){
 	// 体积过大或表面积超过之前的最小值
 	if (v > n || s >= ans)	return ;
 	// 拼尽全力无法完成目标
-	if (v + (lr + m - x) * lr * lh < n)	return;
+	if (v + lr * lr * lh * (m - x + 1) < n)	return;
 	if (x > m){
 		if (v == n)
 			ans = s;
@@ -23,15 +23,14 @@ void dfs(int x,int v,int s){
 	// 圆柱体积：PI*r^2*h
 	// 圆柱侧面积：PI*2*r*h
 	for (int ir = lr - 1;ir > m - x;ir--){
-		if (x == 1)	s = ir * ir;
 		for (int ih = lh - 1;ih > m - x;ih--){
 			v += ir * ir * ih;
-			s += 2 * ir * ih;
+			s += 2 * ir * ih + (x == 1?ir * ir:0);
 			r[x] = ir;
 			h[x] = ih;
 			dfs(x + 1,v,s);
 			v -= ir * ir * ih;
-			s -= 2 * ir * ih;
+			s -= 2 * ir * ih + (x == 1?ir * ir:0);
 			r[x] = 0;
 			h[x] = 0;
 		}
@@ -39,7 +38,7 @@ void dfs(int x,int v,int s){
 }
 int main(int argc, char **argv){
 	cin >> n >> m;
-	h[0] = sqrt(n) + 1,r[0] = sqrt(n) + 1;
+	h[0] = n,r[0] = 28;
 	dfs(1,0,0);
 	if (ans == INF)	cout << 0;
 	else	cout << ans;
