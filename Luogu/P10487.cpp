5e12560f11b715118cb399a44ec1811af070a6b9
huangminzhe <Lin_morning@outlook.com>
2025-03-13 14:00:04 +0800

    暂存P10487Nightmare II

--- Luogu/P10487.cpp
+++ Luogu/P10487.cpp
@@ -7,17 +7,60 @@ struct node{
 int n,m;
 char a[N][N];
 int bx,by,gx,gy;
-void bfs(node b,node g){
-	queue<node> qb,qg;
-	qb.push(b);
-	qg.push(g);
-	while (!qb.empty() && !qg.empty()){
+int gst[N][N],tim[N][N][2];
+int dx[] = {-1,1,0,0},dy[] = {0,0,-1,1};	// 女生方向数组
+int dbx[] = {-1,1,0,0,-2,2,0,0,-1,1,1,-1,-3,3,0,0,-2,2,1,-1,-1,1,2,-2},dby[] = {0,0,-1,1,0,0,-2,2,-1,1,-1,1,0,0,-3,3,-1,1,-2,2,-2,2,-1,1};	// 男生方向数组
+int dgx[] = {-1,1,0,0,-2,2,0,0,-1,1,1,-1},dgy[] = {0,0,-1,1,0,0,-2,2,-1,1,-1,1};	// 怪物方向数组
+void bfsg(node g){
+	queue<node> q;
+	q.push(g);
+	gst[g.x][g.y] = 0;
+	while (!q.empty()){
+		int x = q.front().x,y = q.front().y;
+		q.pop();
+		for (int i = 0;i < 12;i++){
+			int xi = x + dgx[i],yi = y + dgy[i];
+			if (xi < 1 || xi > n || yi < 1 || yi > m || gst[x][y] + 1 >= gst[xi][yi])	continue;
+			gst[xi][yi] = gst[x][y] + 1;
+			q.push({xi,yi});
+		}
 	}
+}
+int bfs(node b,node g){
+	queue<node> q;
+	q.push(g);	// 先搜女生
+	tim[g.x][g.y][0] = 0;
+	while (!q.empty()){
+		int x = q.front().x,y = q.front().y;
+		q.pop();
+		for (int i = 0;i < 4;i++){
+			int xi = x + dx[i],yi = y + dy[i];
+			if (xi < 1 || xi > n || yi < 1 || yi > m || a[xi][yi] == '#' || tim[x][y][0] + 1 <= gst[xi][yi])	continue;
+			tim[xi][yi][0] = tim[x][y][0] + 1;
+			q.push({xi,yi});
+		}
+	}
+	q.push(b);	// 再搜男生
+	tim[b.x][b.y][1] = 0;
+	while (!q.empty()){
+		int x = q.front().x,y = q.front().y;
+		q.pop();
+		if (tim[x][y][1] == tim[x][y][0])	return tim[x][y][0];
+		for (int i = 0;i < 24;i++){
+			int xi = x + dbx[i],yi = y + dby[i];
+			if (xi < 1 || xi > n || yi < 1 || yi > m || a[xi][yi] == '#' || tim[x][y][1] + 1 <= gst[xi][yi])	continue;
+			tim[xi][yi][1] = tim[x][y][1] + 1;
+			q.push({xi,yi});
+		}
+	}
+	return -1;
 }
 int main(int argc, char **argv){
 	int t;
 	cin >> t;
 	while (t--){
+		memset(a,0,sizeof a);
+		memset(gst,0x7f,sizeof gst);
 		cin >> n >> m;
 		for (int i = 1;i <= n;i++){
 			string s;
@@ -33,9 +76,12 @@ int main(int argc, char **argv){
 					gx = i;
 					gy = j;
 				}
+				if (a[i][j] == 'Z'){
+					bfsg({i,j});
+				}
 			}
 		}
-		bfs({bx,by},{gx,gy});
+		cout << bfs({bx,by},{gx,gy}) << '\n';
 	}
 	return 0;
 }
