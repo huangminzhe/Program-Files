593b91a2268de7222341e570f190f2db71063988
huangminzhe <Lin_morning@outlook.com>
2025-03-21 13:48:31 +0800

    在检查C++版本的程序中添加暂停功能

--- checkcppversion.cpp
+++ checkcppversion.cpp
@@ -6,5 +6,6 @@ int main()
     else if (__cplusplus == 201103L) std::cout << "C++11\n";
     else if (__cplusplus == 199711L) std::cout << "C++98\n";
     else std::cout << "pre-standard C++\n";
+	system("pause");
     return 0;
 }
