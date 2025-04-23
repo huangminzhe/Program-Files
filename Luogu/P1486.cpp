f09441d27bff3d58ef442a92792605dd122dbcb0
huangminzhe <Lin_morning@outlook.com>
2025-04-23 14:01:27 +0800

    暂存P1486郁闷的出纳员

---
+++ Luogu/P1486.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<int SIZE = int(1e7) + 5>
+class Treap{
+	private:
+		struct node{
+			int l,r;
+			int k,x;	// k：关键码
+			int cnt,size;	// cnt：个数；size：子树大小
+		}nodes[SIZE];
+		int root,tot,INF = 0x7fffffff;
+		int push(int x){
+			nodes[++tot].k = rand();
+			nodes[tot].x = x;
+			nodes[tot].cnt = 1;
+			nodes[tot].size = 1;
+			return tot;
+		}
+		void upd(int p){
+			nodes[p].size = nodes[nodes[p].l].size + nodes[nodes[p].r].size + nodes[p].cnt;
+		}
+		void ins(int &p,int x){
+			if (!p){
+				p = push(x);	// nodes[fa].l（或r）= p
+				return ;
+			}
+			nodes[p].size++;
+			if (x == nodes[p].x){
+				nodes[p].cnt++;
+				return ;
+			}
+			if (x < nodes[p].x){
+				ins(nodes[p].l,x);
+				if (nodes[p].k < nodes[nodes[p].l].k)	zig(p);
+			}else{
+				ins(nodes[p].r,x);
+				if (nodes[p].k < nodes[nodes[p].r].k)	zag(p);
+			}
+		}
+		void rm(int &p,int x){
+			if (!p)	return ;
+			nodes[p].size--;
+			if (x == nodes[p].x){
+				if (nodes[p].cnt > 1){
+					nodes[p].cnt--;
+					return ;
+				}
+				if (!(nodes[p].l && nodes[p].r))	p = nodes[p].l + nodes[p].r;
+				else if (nodes[nodes[p].l].k > nodes[nodes[p].r].k){
+					zig(p);
+					rm(nodes[p].r,x);
+				}else{
+					zag(p);
+					rm(nodes[p].l,x);
+				}
+				return ;
+			}
+			if (x < nodes[p].x)	rm(nodes[p].l,x);
+			else	rm(nodes[p].r,x);
+		}
+		int v2r(int p,int x){
+			if (!p)	return 1;	// 如果x不在合集，得多返回1（定义）
+			if (x == nodes[p].x)	return nodes[nodes[p].l].size + 1;
+			if (x < nodes[p].x)	return v2r(nodes[p].l,x);
+			return v2r(nodes[p].r,x) + nodes[nodes[p].l].size + nodes[p].cnt;
+		}
+		int r2v(int p,int r){
+			if (!p)	return 0;
+			if (nodes[nodes[p].l].size >= r)	return r2v(nodes[p].l,r);
+			if (nodes[nodes[p].l].size + nodes[p].cnt >= r)	return nodes[p].x;
+			return r2v(nodes[p].r,r - nodes[nodes[p].l].size - nodes[p].cnt);
+		}
+		void zig(int &p){	// 右旋
+			int q = nodes[p].l;
+			nodes[p].l = nodes[q].r,
+			nodes[q].r = p,
+			nodes[q].size = nodes[p].size;
+			upd(p);
+			p = q;
+		}
+		void zag(int &p){	// 左旋
+			int q = nodes[p].r;
+			nodes[p].r = nodes[q].l,
+			nodes[q].l = p,
+			nodes[q].size = nodes[p].size;
+			upd(p);
+			p = q;
+		}
+	public:
+		Treap(){
+			srand(time(0));
+		}
+		void ins(int x){
+			ins(root,x);
+		}
+		void rm(int x){
+			rm(root,x);
+		}
+		int pre(int x){
+			int p = root;
+			int res = -INF;
+			while (p){
+				if (nodes[p].x < x)	res = nodes[p].x,p = nodes[p].r;
+				else	p = nodes[p].l;
+			}
+			return res;
+		}
+		int nxt(int x){
+			int p = root;
+			int res = INF;
+			while (p){
+				if (nodes[p].x > x)	res = nodes[p].x,p = nodes[p].l;
+				else	p = nodes[p].r;
+			}
+			return res;
+		}
+		int find(int x){	// 返回值x的排名（定义为比x小的数的个数+1）
+			return v2r(root,x);
+		}
+		int get(int r){	// 返回排名r的值
+			return r2v(root,r);
+		}
+};
+template<int SIZE = int(1e7) + 5>
+class p_Treap:public Treap<SIZE>{
+	private:
+		int mn,cnt = 0;
+		void set(int &p,int x){
+			if (nodes[p].x >= mn){
+				nodes[p].x += x;
+				if (nodes[p].x < mn)	cnt++;
+			}
+			if (nodes[p].l)	add(nodes[p].l,x);
+			if (nodes[p].r)	add(nodes[p].r,x);
+		}
+		int topk(int p,int k){
+			if (!p)	return 0;
+			if ()
+		}
+	public:
+		p_Treap(int m){
+			mn = m;
+			srand(time(0));
+		}
+		void set(int x){
+			set(root,x);
+		}
+		int get_lv(){	// 离开的人数
+			return cnt;
+		}
+		int topk(int k){
+			return topk(root,k);
+		}
+};
+int main(int argc, char **argv){
+	return 0;
+}
