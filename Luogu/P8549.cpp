07480a98941b8e241159afb36e3c661fb737ee16
huangminzhe <Lin_morning@outlook.com>
2025-05-07 14:01:06 +0800

    暂存P8549另类数独问题 // check() 有问题

--- Luogu/P8549.cpp
+++ Luogu/P8549.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 0x7fffffff;
-bool x[20][20],y[20][20];
 int z[5][5][20],mn = INF;
 int a[20][20],n;
-void zig(int u,int v){
+pair<int,int> p[50],ans[50];
+void zig(int x,int y){
 	int tmp[5][5] = {{0}},
-		up = u * n - n + 1,dwn = u * n,
-		lft = v * n - n + 1,rt = v * n;
+		up = x * n - n + 1,dwn = x * n,
+		lft = y * n - n + 1,rt = y * n;
 	for (int i = 1;i <= n;i++){
 		for (int j = 1;j <= n;j++){
 			tmp[n - j + 1][i] = a[up + i - 1][lft + j - 1];
@@ -18,6 +18,7 @@ void zig(int u,int v){
 			a[up + i - 1][lft + j - 1] = tmp[i][j];
 		}
 	}
+	printf("%d %d\n",x,y);
 	for (int i = 1;i <= n;i++){
 		for (int j = 1;j <= n;j++){
 			printf("%d ",tmp[i][j]);
@@ -25,9 +26,33 @@ void zig(int u,int v){
 		cout << '\n';
 	}
 }
-void dfs(int u,int v,int cnt){
+bool check(int u,int v){
+	int up = u * n - n + 1,dwn = u * n,
+		lft = v * n - n + 1,rt = v * n;
+	bool x[20][20] = {{0}},y[20][20] = {{0}};
+	for (int i = 1;i <= dwn;i++){
+		for (int j = 1;j <= rt;j++){
+			if (x[i][a[i][j]] || y[j][a[i][j]])
+				return 0;
+			x[i][a[i][j]] = y[j][a[i][j]] = 1;
+		}
+	}
+	return 1;
+}
+void dfs(int x,int y,int cnt){
 	if (cnt >= mn)	return ;
-	if 
+	if (x > n){
+		mn = cnt;
+		for (int i = 1;i <= cnt;i++)
+			ans[i] = p[i];
+		return ;
+	}
+	for (int i = 0;i < 4;i++){
+		p[cnt + i] = {x,y};
+		if (check(x,y))
+			dfs((y == n?x + 1:x),((y == n?1:y + 1)),cnt + i);
+		zig(x,y);
+	}
 }
 int main(int argc, char **argv){
 	cin >> n;
@@ -49,8 +74,9 @@ int main(int argc, char **argv){
 	}
 	dfs(1,1,0);
 	if (mn == INF)	mn = -1;
-	cout << mn;
+	cout << mn << '\n';
 	for (int i = 1;i <= mn;i++){
+		cout << ans[i].first << ' ' << ans[i].second << '\n';
 	}
 	return 0;
 }
