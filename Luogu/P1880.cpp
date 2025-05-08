d899eb90e07237c6efce95b49702d9084b0c0fe2
huangminzhe <Lin_morning@outlook.com>
2025-05-08 13:39:58 +0800

    完成P1880石子合并

--- Luogu/P1880.cpp
+++ Luogu/P1880.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 105;
-int a[N * 2],dp[N][N],b[N * 2];
+const int N = 105,INF = 0x7fffffff;
+int a[N * 2],dpn[N * 2][N * 2],dpx[N * 2][N * 2],b[N * 2],mn = INF,mx;
 int main(int argc, char **argv){
-	memset(dp,0x3f,sizeof(dp));
+	memset(dpn,0x3f,sizeof(dpn));
 	int n;
 	cin >> n;
 	for (int i = 1;i <= n;i++){
@@ -12,16 +12,21 @@ int main(int argc, char **argv){
 	}
 	for (int i = 1;i <= n * 2;i++){
 		b[i] = b[i - 1] + a[i];
-		dp[i][i] = 0;
+		dpn[i][i] = 0;
 	}
 	for (int k = 1;k < n;k++){
-		for (int i = 1;i <= n - k;i++){
+		for (int i = 1;i <= 2 * n - k;i++){
 			int j = i + k;
 			for (int k = i;k < j;k++){
-				dp[i][j] = min(dp[i][j],dp[i][k] + dp[k + 1][j] + b[j] - b[i - 1]);
+				dpn[i][j] = min(dpn[i][j],dpn[i][k] + dpn[k + 1][j] + b[j] - b[i - 1]);
+				dpx[i][j] = max(dpx[i][j],dpx[i][k] + dpx[k + 1][j] + b[j] - b[i - 1]);
 			}
 		}
 	}
-	cout << dp[1][n];
+	for (int i = 1;i <= n;i++){
+		mn = min(mn,dpn[i][i + n - 1]);
+		mx = max(mx,dpx[i][i + n - 1]);
+	}
+	cout << mn << '\n' << mx;
 	return 0;
 }
