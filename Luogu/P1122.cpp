91dcf739c325c8bdbfdca95c06379e554765e7bc
huangminzhe <Lin_morning@outlook.com>
2025-05-16 21:34:20 +0800

    完成P1122最大子树和

---
+++ Luogu/P1122.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1.6e4 + 5,INF = 0x7fffffff;
+int n,dp[N],a[N],mx = -INF;
+vector<int> g[N];
+void dfs(int u,int fa){
+	dp[u] = a[u];
+	for (int v : g[u]){
+		if (v == fa)	continue;
+		dfs(v,u);
+		if (dp[v] > 0)	dp[u] += dp[v];
+	}
+}
+int main(int argc, char **argv){
+	cin >> n;
+	for (int i = 1;i <= n;i++){
+		cin >> a[i];
+	}
+	for (int i = 1;i < n;i++){
+		int u,v;
+		cin >> u >> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	dfs(1,0);
+	for (int i = 1;i <= n;i++){
+		mx = max(mx,dp[i]);
+	}
+	cout << mx;
+	return 0;
+}
