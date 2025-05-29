b0caf72540ec4d0bd3667b3a73926c021a8e75f7
huangminzhe <Lin_morning@outlook.com>
2025-05-29 14:00:44 +0800

    暂存P1273有线电视网

--- Luogu/P1273.cpp
+++ Luogu/P1273.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int N = 3e3 + 5;
-int n,m,a[N],dfsx[N],sz[N],tot,dp[N][N];
+typedef pair<int,int> pii;
+const int N = 3e3 + 5,INF = 0x3fffffff;
+int n,m,a[N] = {0},dfsx[N],sz[N],tot,dp[N][N],fa[N];
 vector<int> g[N];
 void init(int u){
 	sz[u] = 1,dfsx[u] = ++tot;
@@ -17,22 +18,26 @@ int main(int argc, char **argv){
 		int k;
 		cin >> k;
 		for (int j = 1;j <= k;j++){
-			int v,w;
-			cin >> v >> a[i];
+			int v;
+			cin >> v >> fa[v];
 			g[i].push_back(v);
 		}
 	}
 	for (int i = n - m + 1;i <= n;i++){
-		cin >> a[i];
+		int x;
+		cin >> x;
+		a[i] = x;
 	}
 	init(1);
-	for (int i = n;i >= 1;i--){
+	for (int i = 1;i <= n + 1;i++)	// n+1是边界
+		for (int j = 1;j <= m;j++)
+			dp[i][j] = -INF;
+	for (int i = n;i > 0;i--){
 		for (int j = 1;j <= m;j++){
-			if (i >= n - m + 1){
-				dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j - 1] + a[dfsx[i]]);
-			}else{
-				dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j - 1] - a[dfsx[i]]);
-			}
+			// dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j - (dfsx[i] >= n - m + 1?1:0)] + a[dfsx[i]] - fa[dfsx[i]]);
+			if (dfsx[i] >= n - m + 1)	dp[i][j] = max(dp[i + 1][j],dp[i + 1][j - 1] + a[dfsx[i]] - fa[dfsx[i]]);
+			else	dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j] - fa[dfsx[i]]);
+			// printf("%d %d %d %d %d\n",i,j,dp[i][j],dp[i + sz[dfsx[i]]][j],dp[i + 1][j - (dfsx[i] >= n - m + 1?1:0)] + a[dfsx[i]] - fa[dfsx[i]]);
 		}
 	}
 	for (int i = m;i >= 0;i--){
