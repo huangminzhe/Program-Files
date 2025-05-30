e3cbad7cd2f95eaee83c589d888c48ee101ae66a
huangminzhe <Lin_morning@outlook.com>
2025-05-30 13:47:32 +0800

    完成P1273有线电视网

--- Luogu/P1273.cpp
+++ Luogu/P1273.cpp
@@ -3,10 +3,10 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> pii;
 const int N = 3e3 + 5,INF = 0x3fffffff;
-int n,m,a[N] = {0},dfsx[N],sz[N],tot,dp[N][N],fa[N];
+int n,m,a[N],dfsx[N],sz[N],tot,dp[N][N],fa[N];
 vector<int> g[N];
 void init(int u){
-	sz[u] = 1,dfsx[u] = ++tot;
+	sz[u] = 1,dfsx[++tot] = u;
 	for (int v : g[u]){
 		init(v);
 		sz[u] += sz[v];
@@ -34,11 +34,10 @@ int main(int argc, char **argv){
 			dp[i][j] = -INF;
 	for (int i = n;i > 0;i--){
 		for (int j = 1;j <= m;j++){
-			// dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j - (dfsx[i] >= n - m + 1?1:0)] + a[dfsx[i]] - fa[dfsx[i]]);
-			if (dfsx[i] >= n - m + 1)	dp[i][j] = max(dp[i + 1][j],dp[i + 1][j - 1] + a[dfsx[i]] - fa[dfsx[i]]);
-			else	dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j] - fa[dfsx[i]]);
+			dp[i][j] = max(dp[i + sz[dfsx[i]]][j],dp[i + 1][j - (dfsx[i] >= n - m + 1?1:0)] + a[dfsx[i]] - fa[dfsx[i]]);
 			// printf("%d %d %d %d %d\n",i,j,dp[i][j],dp[i + sz[dfsx[i]]][j],dp[i + 1][j - (dfsx[i] >= n - m + 1?1:0)] + a[dfsx[i]] - fa[dfsx[i]]);
 		}
+		// printf("%d %d\n",i,dp[i][m]);
 	}
 	for (int i = m;i >= 0;i--){
 		if (dp[1][i] >= 0){
