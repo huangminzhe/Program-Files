136919436d992ba3ea9fd8a5a2b3625c351d7749
huangminzhe <Lin_morning@outlook.com>
2025-06-30 14:41:07 +0800

    完成P3959宝藏

--- Luogu/P3959.cpp
+++ Luogu/P3959.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int N = 15,M = 1 << 12,INF = 0x3fffffff;
-int n,m,a[N][N],d[M][M];
-ll dp[N][M],ans;
+const int N = 15,M = 1 << 12,INF = 0x3f3f3f3f;
+int n,m,a[N][N];
+ll d[M][M],dp[N][M],ans;
 int lowbit(int x){
 	return x & -x;
 }
@@ -12,10 +12,12 @@ int lowwei(int x){
 }
 int main(int argc, char **argv){
 	cin >> n >> m;
+	memset(a,0x3f,sizeof a);
 	for (int i = 1;i <= m;i++){
 		int u,v,w;
 		cin >> u >> v >> w;
-		a[u][v] = min(!a[u][v]?w:a[u][v],w);
+		a[u][v] = min(a[u][v],w);
+		// a[u][v] = min(!a[u][v]?w:a[u][v],w);
 		a[v][u] = a[u][v];
 	}
 	for (int i = 1;i < 1 << n;i++){
@@ -35,11 +37,11 @@ int main(int argc, char **argv){
 	}
 	memset(dp,0x3f,sizeof dp);
 	for (int i = 0;i < n;i++)
-		dp[1][1 << i] = 0;
-	ans = dp[1][(1 << n) - 1];
-	for (int i = 2;i <= n;i++){
+		dp[0][1 << i] = 0;
+	ans = dp[0][(1 << n) - 1];
+	for (int i = 1;i <= n;i++){
 		for (int j = 1;j < 1 << n;j++){
-			if (!(j & 1 << i - 1))	continue;
+			// if (!(j & 1 << i - 1))	continue;
 			for (int k = j;k;k = k - 1 & j){
 				if (k == j)	continue;
 				dp[i][j] = min(dp[i][j],dp[i - 1][j - k] + i * d[j - k][k]);
