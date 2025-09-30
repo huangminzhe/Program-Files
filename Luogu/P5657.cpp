d4211ceb3f2b94f98b3fdb8b997a9f6125db9372
huangminzhe <Lin_morning@outlook.com>
2025-09-30 13:30:12 +0800

    完成P5657格雷码

--- Luogu/P5657.cpp
+++ Luogu/P5657.cpp
@@ -1,25 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
+int o[] = {0,1,1,0};
 ull qpow(int x,int y){
 	if (!y)	return 1;
 	ull a = qpow(x,y / 2);
 	return a * a * (y & 1?x:1);
 }
 int main(int argc, char **argv){
-	int n;ull k,p;
+	int n;ull k;
 	cin >> n >> k;
-	n--;
-	p = qpow(2,n);
-	bool flag = k >= p;
-	while (n){
-		n--;
-		cout << flag;
-		printf(" %llu ",p);
-		if (flag)	k -= p,k = p - k - 1;
-		p = qpow(2,n);
-		flag = flag?k >= p:k < p;
+	while (n--){
+		cout << o[k / qpow(2,n) % 4];
 	}
-	cout << flag;
 	return 0;
 }
