5c32ea24695265536e17313bb076904ad98a592b
huangminzhe <Lin_morning@outlook.com>
2025-10-20 14:03:14 +0800

    暂存P1522Cow Party S

--- Luogu/P1522.cpp
+++ Luogu/P1522.cpp
@@ -1,33 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 155;
+
 struct point{
-	int v;
-	double w;
+	double x,y;
 }a[N];
-bool operator< (point b,point c){
-	return b.w > c.w;
+
+int fa[N];
+int find(int x){
+	if (fa[x] == x)	return x;
+	return fa[x] = find(fa[x]);
 }
-vector<point> g[N];
+void merge(int x,int y){
+	fa[find(y)] = find(x);
+}
+
 int n;
+double d[N][N];
 inline double calc(int u,int v){
-	return sqrt((a[u].w - a[v].w) * (a[u].w - a[v].w) + (a[u].v - a[v].v) * (a[u].v - a[v].v));
+	return sqrt((a[u].y - a[v].y) * (a[u].y - a[v].y) + (a[u].x - a[v].x) * (a[u].x - a[v].x));
 }
 int main(int argc, char **argv){
 	cin >> n;
 	for (int i = 1;i <= n;i++){
-		cin >> a[i].v >> a[i].w;
+		cin >> a[i].x >> a[i].y;
 	}
+	for (int i = 1;i <= n;i++)	fa[i] = i;
 	for (int i = 1;i <= n;i++){
 		string s;
 		cin >> s;
 		for (int j = 1;j <= n;j++){
 			int sj = j - 1;
 			if (s[sj] - '0'){
-				g[i].push_back({j,calc(i,j)});
+				d[i][j] = calc(i,j);
+				merge(i,j);
 			}
 		}
 	}
-	
+	for (int k = 1;k <= n;k++)
+		for (int i = 1;i <= n;i++)
+			for (int j = 1;j <= n;j++)
+				d[i][j] = min(d[i][j],d[i][k] + d[k][j]);
 	return 0;
 }
