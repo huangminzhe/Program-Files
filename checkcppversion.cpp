fc0c61ab22fa41d6c0ef10e95a9b61cb1206ed40
huangminzhe <Lin_morning@outlook.com>
2025-10-22 19:30:00 +0800

    更改c++版本检测文件

--- checkcppversion.cpp
+++ checkcppversion.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 int main()
 {
-    if (__cplusplus == 201703L) std::cout << "C++17\n";
+	if (__cplusplus == 202302L)	std::cout << "C++23\n";
+    else if (__cplusplus == 201703L) std::cout << "C++17\n";
     else if (__cplusplus == 201402L) std::cout << "C++14\n";
     else if (__cplusplus == 201103L) std::cout << "C++11\n";
     else if (__cplusplus == 199711L) std::cout << "C++98\n";
-    else std::cout << "pre-standard C++\n";
+    else std::cout << __cplusplus << '\n';
 	system("pause");
     return 0;
 }
