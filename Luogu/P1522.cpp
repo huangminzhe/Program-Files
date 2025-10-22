58b8c3a4735fc9c06929dc259e9316f6eef55d47
huangminzhe <Lin_morning@outlook.com>
2025-10-22 13:02:20 +0800

    完成P1522牛的旅行

--- Luogu/P1522.cpp
+++ Luogu/P1522.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 155;
+const double INF = DBL_MAX;
 
 struct point{
 	double x,y;
@@ -16,7 +17,7 @@ void merge(int x,int y){
 }
 
 int n;
-double d[N][N];
+double d[N][N],dia[N],mx[N],ans = INF;
 inline double calc(int u,int v){
 	return sqrt((a[u].y - a[v].y) * (a[u].y - a[v].y) + (a[u].x - a[v].x) * (a[u].x - a[v].x));
 }
@@ -25,13 +26,13 @@ int main(int argc, char **argv){
 	for (int i = 1;i <= n;i++){
 		cin >> a[i].x >> a[i].y;
 	}
-	for (int i = 1;i <= n;i++)	fa[i] = i;
+	for (int i = 1;i <= n;i++)	fa[i] = i,fill(d[i] + 1,d[i] + n + 1,INF / 2);
 	for (int i = 1;i <= n;i++){
 		string s;
 		cin >> s;
 		for (int j = 1;j <= n;j++){
 			int sj = j - 1;
-			if (s[sj] - '0'){
+			if (s[sj] - '0' || i == j){
 				d[i][j] = calc(i,j);
 				merge(i,j);
 			}
@@ -41,5 +42,21 @@ int main(int argc, char **argv){
 		for (int i = 1;i <= n;i++)
 			for (int j = 1;j <= n;j++)
 				d[i][j] = min(d[i][j],d[i][k] + d[k][j]);
+	for (int i = 1;i <= n;i++){
+		for (int j = 1;j <= n;j++){
+			if (d[i][j] != INF / 2)
+				mx[i] = max(mx[i],d[i][j]);
+		}
+		dia[find(i)] = max(dia[find(i)],mx[i]);
+	}
+	for (int i = 1;i <= n;i++){
+		for (int j = 1;j <= n;j++){
+			if (find(i) != find(j)){
+				ans = min(ans,
+						  max({dia[find(i)],dia[find(j)],mx[i] + calc(i,j) + mx[j]}));
+			}
+		}
+	}
+	printf("%.6f",ans);
 	return 0;
 }
