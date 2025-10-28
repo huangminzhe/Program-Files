95f592501467708326159beff369dfb35bfcae13
huangminzhe <Lin_morning@outlook.com>
2025-10-28 14:01:30 +0800

    暂存ZXOJ-LGT1007走迷宫

--- ZXOJ/T1007.cpp
+++ ZXOJ/T1007.cpp
@@ -9,8 +9,16 @@ set<int> blkx[N],blky[N];	// x=i的障碍物；y=i的障碍物
 map<pii,int> id;	// 关键点编号
 vector<int> g[N * 4];
 
+pii check_left(pii p){;
+	int x = p.first,y = p.second;
+	auto it = lower_bound(blkx[x].begin(),blkx[x].end(),y);
+	if (*it >= y)	return p;
+}
+pii (*fdnxt[4])(pii) = {};
+
 int main(int argc, char **argv){
 	cin >> n >> m >> sx >> sy;
+	// 输入，存障碍物
 	for (int i = 1;i <= n;i++){
 		int x,y;
 		cin >> x >> y;
@@ -18,6 +26,7 @@ int main(int argc, char **argv){
 		blkx[x].insert(y);
 		blky[y].insert(x);
 	}
+	// 根据障碍物存关键点
 	pts.insert({sx,sy});
 	for (auto [x,y] : blks){
 	// for (auto i : blks){
@@ -27,13 +36,16 @@ int main(int argc, char **argv){
 			if (blks.find({xi,yi}) == blks.end())	pts.insert({xi,yi});
 		}
 	}
+	// 给关键点编号
 	for (auto i : pts){
-		int x = i.first,y = i.second;
 		id[i] = ++tot;
+	}
+	// 建图
+	for (auto i : blks){
+		int x = i.first,y = i.second;
 		for (int I = 0;I < 4;I++){
 			int xi = x + dx[I],yi = y + dy[I];
-			if (blks.find({xi,yi}) == blks.end()){
-			}
+			g[id[i]].push_back();
 		}
 	}
 	return 0;
