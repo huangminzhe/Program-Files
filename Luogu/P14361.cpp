965dfcfdfe5194b73efd49824215511b53f47e8d
huangminzhe <Lin_morning@outlook.com>
2025-11-20 13:31:54 +0800

    完成P14361社团招新

--- Luogu/P14361.cpp
+++ Luogu/P14361.cpp
@@ -9,6 +9,8 @@ int main(int argc, char **argv){
 	int t;
 	cin >> t;
 	while (t--){
+		ans = cnt1 = cnt2 = cnt3 = 0;
+		
 		cin >> n;
 		for (int i = 1;i <= n;i++){
 			cin >> a[i].a >> a[i].b >> a[i].c;
@@ -22,6 +24,26 @@ int main(int argc, char **argv){
 				b[i] = 3,ans += a[i].c,cnt3++;
 			}
 		}
+		priority_queue<int,vector<int>,greater<>> q;
+		for (int i = 1;i <= n;i++){
+			if (cnt1 > n / 2 && b[i] == 1){
+				q.push(a[i].a - max(a[i].b,a[i].c));
+			}else if (cnt2 > n / 2 && b[i] == 2){
+				q.push(a[i].b - max(a[i].a,a[i].c));
+			}else if (cnt3 > n / 2 && b[i] == 3){
+				q.push(a[i].c - max(a[i].a,a[i].b));
+			}
+		}
+		while (cnt1 > n / 2){
+			ans -= q.top(),q.pop(),cnt1--;
+		}
+		while (cnt2 > n / 2){
+			ans -= q.top(),q.pop(),cnt2--;
+		}
+		while (cnt3 > n / 2){
+			ans -= q.top(),q.pop(),cnt3--;
+		}
+		cout << ans << '\n';
 	}
 	return 0;
 }
