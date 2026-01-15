8ccda1b8529d6ef353158bb5c78e86fb67194fb4
huangminzhe <Lin_morning@outlook.com>
2026-01-15 13:41:43 +0800

    New Luogu v1.1暂存

--- New Luogu/New Luogu.js
+++ New Luogu/New Luogu.js
@@ -95,7 +95,7 @@
 				lang = 'cpp';
 			}
 			const ln = '				<span style="height: 21px;"></span>\n'.repeat(countLine(code));
-			code.replace(/[<>&"']/g,function(m){
+			const tcode = code.replace(/[<>&"']/g,function(m){
 				switch (m){
 					case '&':	return '&amp;';
 					case '<':	return '&lt;';
@@ -108,20 +108,15 @@
 
 			let res = `
 <div data-v-a7061ca4="" class="code-container">
-	<pre data-v-a7061ca4="" data-line="" class="pre line-numbers ${n?' ':'hide-numbers '}language-${lang}" tabindex="0">
-		<code data-v-a7061ca4="" class="language-${lang}">
-			${code}
-			<span aria-hidden="true" class="line-numbers-rows">
+<pre data-v-a7061ca4="" data-line="" class="pre line-numbers ${n?' ':'hide-numbers '}language-${lang}" tabindex="0">
+<code data-v-a7061ca4="" class="language-${lang}">
+${tcode}
+<span aria-hidden="true" class="line-numbers-rows">
 ${ln}
-			</span>
-			<span class="line-numbers-sizer" style="display: none;"></span>
-		</code>
-	</pre>
-	<button data-v-a7061ca4="" type="button" class="copy-button">
-		<svg data-v-a7061ca4="" class="svg-inline--fa fa-copy copy-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="copy" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-			<path class="" fill="currentColor" d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"></path>
-		</svg>
-	</button>
+</span>
+<span class="line-numbers-sizer" style="display: none;"></span>
+</code>
+</pre>
 </div>
 `;
 			console.log('代码块 res 调试:',res);
